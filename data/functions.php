2f826d53b53a1c5f7058e201e3218e12c55957ce
Draed <draedper@gmail.com>
2016-06-28 11:10:41 +0400

    13th commit
    change the merge PB

--- data/functions.php
+++ data/functions.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
 * Get all the files from current folder. 
 * For data or test
@@ -7,22 +6,18 @@
 */
 function getAllApps(){
 	$dir1 = scandir('.');
-
     foreach($dir1 as $element) {
 	    if (is_dir($element) && $element != '.' && $element != '..') {
 	        echo '<option value="'.$element .'">'.$element .'</option>';
 	    }
     }
 }
-
-
-Function getAllEnv() {
+function getAllEnv() {
     $Env= array("integration","integration2","preprod","production");
     foreach($Env as $element) {
         echo '<option value="'.$element.'">'.$element.'</option>';
     }
 }
-
 /**
 * Get all the files of a selected application's folder.
 * Only for data folder 
@@ -53,53 +48,41 @@ function getDatasFromFolder() {
 	}
     return $html;
 }
-
-
-
-
+function listeNombrefichier($application) {
+    $nb_fichier = 0;
+    if($dossier = opendir($application)) {
+        while(false !== ($fichier = readdir($dossier))){
+            if($fichier != '.' && $fichier != '..' && $fichier != 'index.php' && $fichier != '.DS_Store' && $fichier != 'template.csv'){                    
+                $nb_fichier++;
+            }            
+        }
+        closedir($dossier); 
+        return $nb_fichier+1;        
+    } else {
+        // return false;
+    }
+}  
 function importData() {
     //get variables
     if (isset($_POST['application'])) {
         $application=htmlentities($_POST['application']);
+        $nombreFichier = listeNombrefichier($application);
     }
     if (isset($_POST['environnement'])) {
-    $environnement=htmlentities($_POST['environnement']);
+        $environnement=htmlentities($_POST['environnement']);
     }
-    //function 
-    function listeNombrefichier() {
-        if (isset($_POST['application'])) {
-            $application=htmlentities($_POST['application']);
-        }
-        $nb_fichier = 0;
-        if($dossier = opendir($application)) {
-            while(false !== ($fichier = readdir($dossier))){
-                if($fichier != '.' && $fichier != '..' && $fichier != 'index.php' && $fichier != '.DS_Store' && $fichier != 'template.csv'){                    
-                    $nb_fichier++;
-                }            
-            }      
-                return $nb_fichier+1;
-            closedir($dossier);
-        } else {
-
-        }
-    }       
-    //var;
-    $nombreFichier = listeNombrefichier($application);
     $maxsize="4096";
     $extensions_valides = array( 'xml' );
     $tmp_file = $_FILES['file']['tmp_name'];
     $name_file = $environnement.".dataset.".$nombreFichier.".xml";
     $content_dir = $application;
-
     //test
     if( !is_uploaded_file($tmp_file) ) exit("Le fichier est introuvable");
     if ($_FILES['file']['error'] > 0) echo $erreur = "Erreur lors du transfert";
     if ($_FILES['file']['size'] > $maxsize) echo $erreur = "la taille du fichier est trop importante";
     if ($_FILES['file']['size'] = 0) echo $erreur = "le fichier est vide";
-
     $extension_upload = strtolower(  substr(  strrchr($_FILES['file']['name'], '.')  ,1)  );
     if ( in_array($extension_upload,$extensions_valides) );
-
     if( !move_uploaded_file($tmp_file, $content_dir."/".$name_file) ) echo("Impossible de copier".$name_file." dans ".$content_dir."<br>");
     else echo "le fichier ".$name_file." a bien été copié vers le dossier ".$content_dir."<br>"; 
 }
